93237fda75e29eb6e9fc6921735c46eff21e2792
emeeran <emeeran@hotmail.com>
2024-04-11 22:42:31 +0530

    simple message sends to numbers

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import pywhatkit
+import time
+from selenium import webdriver
+
+# Function to read contacts from a file
+def read_contacts(filename):
+    with open(filename, 'r') as file:
+        contacts = [line.strip() for line in file]
+    return contacts
+
+# Function to send WhatsApp message
+def send_whatsapp_message(number, message):
+    pywhatkit.sendwhatmsg_instantly(number, message, tab_close=True)
+
+# Main function
+def main():
+    contacts = read_contacts('contacts.txt')
+    message = "Hello, This is an automated message"
+    for contact in contacts:
+        send_whatsapp_message(contact, message)
+        time.sleep(30)  # wait for 10 seconds before sending the next message
+
+if __name__ == "__main__":
+    main()
